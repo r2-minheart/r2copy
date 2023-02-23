791aef6181b4f3e82b3fe0289529aca549c61dc6
wowjini <wowjini@gmail.com>
2023-02-23 11:33:21 +0900

    cron enable

--- r2copy/src/main/java/com/r2comms/r2copy/R2copyApplication.java
+++ r2copy/src/main/java/com/r2comms/r2copy/R2copyApplication.java
@@ -6,8 +6,14 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaAuditing
 public class R2copyApplication {
 
 	public static void main(String[] args) {
