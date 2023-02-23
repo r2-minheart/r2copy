1d9eb649945d3ad2b28a1785b465349dd0c99d2b
wowjini <wowjini@gmail.com>
2023-02-23 10:47:49 +0900

    first commit

---
+++ r2copy/src/main/java/com/r2comms/r2copy/R2copyApplication.java
@@ -0,0 +1,20 @@
+package com.r2comms.r2copy;
+
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2copyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2copyApplication.class, args);
+	}
+    @PostConstruct
+    public void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+}
---
+++ r2copy/src/main/java/com/r2comms/r2copy/config/WebSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.r2comms.r2copy.config;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.r2comms.r2copy.security.filter.ApiCheckFilter;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {		
+		http.csrf().disable();		
+		http
+			.authorizeRequests()			
+				.antMatchers(											
+						"/admin/**").permitAll()
+				.antMatchers("/", "/job/**", "/node/**", "/user/**").hasRole("ADMIN")						
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+					.loginPage("/login")
+					 .defaultSuccessUrl("/")
+					.permitAll()
+					.and()
+				.logout()			  
+			      	.invalidateHttpSession(true).deleteCookies("JSESSIONID")
+					.permitAll()
+					.and()
+
+			    .rememberMe().tokenValiditySeconds(7776000).key("r2commsapp1")   // 60 * 60 * 24 * 7 : 604800,  60*60*24*30일 = 2592000   60*60*24*30일*3개월 = 7776000
+			    .rememberMeParameter("remember-me");
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+		
+	  throws Exception {
+	    auth.jdbcAuthentication()
+	      .dataSource(dataSource)
+	      // .passwordEncoder(passwordEncoder())
+	      .usersByUsernameQuery("select login_id, password, is_active from user where login_id = ?")
+	      .authoritiesByUsernameQuery("select u.login_id, r.role_name from user u inner join role r on u.role_id = r.role_id where u.login_id = ? ");
+	}	
+	
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//	    return new BCryptPasswordEncoder();
+//	}
+	
+	@Bean
+	public ApiCheckFilter apiCheckFilter() {
+		return new ApiCheckFilter("/api/v1/**");
+	}
+    
+}
