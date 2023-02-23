bd4bd6596324f594bcb526feb0824dc78a49d83e
wowjini <wowjini@gmail.com>
2023-02-23 11:24:47 +0900

    first commit

---
+++ r2copy/src/main/java/com/r2comms/r2copy/R2copyApplication.java
@@ -0,0 +1,13 @@
+package com.r2comms.r2copy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2copyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2copyApplication.class, args);
+	}
+
+}
